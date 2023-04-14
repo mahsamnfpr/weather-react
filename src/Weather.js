fc52f74bc6163baa21a92367339daff4a6305ea2
mahsamnfpr <m.manafpoor@gmail.com>
2023-04-14 21:45:45 +0430

    add weather app api and fix it

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+// export default function Weather() {
 //   let weatherData = {
 //     city: "New York",
 //     date: "Tuesday 10:00",
@@ -12,14 +13,37 @@ export default function Weather() {
 //     imgUrl:
 //       "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAg5JREFUeNrt2tGRgyAQBmBLoARLSAmUkBIsISVYgiVYgiVQgq//myXQAfdwS4bJqRFuCWjWmZ3JaGYin8AumMY513xzNAIgAAIgAAIgAAIgAAIgAALwe4LhANACGADMANxLzHStbQoc2QGoce5g9JcBAKA2nvi7mAGoUwP8o/FPhOoBAGgAIwBD0ftxHNntiw6HJICdBiqa8BxD2E9MjNEAAO4bNzwxPn0fA/WsbNkjBWDZuNkHXZ8ZAQ5DfQQAwG3nJjR9xxWKpOwRC6D3AGgOcGdCYAMI0p8rHEMWAGq82Zit++B7roJoWQEAdBs/NPouR/PDUAnAwAaw0+27YKEzVdLw6EryCIDZabymIeBqC06AP93+QEa4LMASlLv2GwF81zc1N54bwJe29kA1WHMY/wBjAe4vY388KUBYLbY5FkNniudSOwXAXSTmlLWAKtRl/a4T91K7qxlgXKvpKQVzzUOmxiFgg70FBeBBu0I9fVacVWiNk6AOFmB2A6gLUrIO9wAoa5mcADnT4Phm9blWlClC0B6Pzve5AHIWQm1EmW1pSNiVcv129GGlboubXGmJGsWS56l37GFOqQA5FkMmputGDKeRLQ1+4mAEWKp4OxzR6KXEirA4QOZ9RPPuHWNRAMb3iMmbpKUBdOkNktIAt+oB5F9iAiAAAiAAAiAAAiAAAiAAAiAAAiAAl48fFVnRpiVnD+AAAAAASUVORK5CYII="
 
+export default function Weather() {
+    const [city, setCity] = useState("");
+    const [loaded, setloaded] = useState(false);
+    const [weather, setWeather] = useState({});
+  
+    function displayWeather(response) {
+      setloaded(true);
+      setWeather({
+        temperature: response.data.main.temp,
+        wind: response.data.wind.speed,
+        humidity: response.data.main.humidity,
+        description: response.data.weather[0].description,
+        icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      });
+    }
+
+function submit(event) {
+    event.preventDefault();
+    let apiKey = "203fa770242fcd2b9555d832a88ea567";
+    let units = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(url).then(displayWeather);
+}
+
+function updateCity(event) {
+    setCity(event.target.value);
+  }
 
 
-
-
-  };
-  return (
-    <div className="weather">
-      <form className="search-form" id="search-form">
+let form = (
+    <form className="search-form" onSubmit={submit}>
         <div className="row">
           <div className="col-8">
             <input
@@ -28,6 +52,7 @@ export default function Weather() {
               autofocus="on"
               autocomplete="off"
               class="form-control shadow-sm"
+              onChange={updateCity} 
             />
           </div>
           <div className="col-4">
@@ -35,37 +60,39 @@ export default function Weather() {
               type="submit"
               value="Search"
               className="form-control btn btn-primary shadow-sm"
+
             />
           </div>
         </div>
       </form>
-      <h1>{weatherData.city}</h1>
-      <ul>
-        <li>{weatherData.date}</li>
-        <li></li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src={weatherData.imgUrl}
-            alt={weatherData.description}
-            className="weather-icon"
-          />
-          <span className="temperature">{weatherData.temp} </span>
-          <span className="unit">°C </span>
-        </div>
-        <div className="col-6">
-          <ul>
+      
+)
+
+if (loaded){
+    return(
+        <div className="weather">
+            {form}
+            <h1>{city}</h1>
+            <ul>
+          <li>Temperature: {Math.round(weather.temperature)} °C</li>
+          
+            <li>Description: {weather.description}</li>
             <li>
-              Humidity: <span></span>
-            </li>
-            <li>
-              Wind: <span></span> km/h
-            </li>
-          </ul>
+                Humidity: {weather.humidity}%
+          </li>
+          <li>Wind: {Math.round(weather.wind)}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+        <br/>
+        <br/>
         </div>
-      </div>
-      <div className="weather-forcast"></div>
-    </div>
-  );
+        
+    );
+}else{
+    return form;
+}
+  
+ 
 }
